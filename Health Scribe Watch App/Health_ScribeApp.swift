3e13269684669b7069ddf83b4c5a4450c2150d6a
Jake Koperski <wijakoperski@gmail.com>
2023-05-27 17:30:03 -0500

    Initial Commit

---
+++ Health Scribe Watch App/Health_ScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_ScribeApp.swift
+//  Health Scribe Watch App
+//
+//  Created by Jake Koperski on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Health_Scribe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Health Scribe/Health_ScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_ScribeApp.swift
+//  Health Scribe
+//
+//  Created by Jake Koperski on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Health_ScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
